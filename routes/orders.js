3ec323e3b5773089c925551158afa6a4f3e1f5d1
JOSE MATOS <josemmatosd@gmail.com>
2025-02-02 16:57:22 -0400

    fix: cache

--- routes/orders.js
+++ routes/orders.js
@@ -16,6 +16,17 @@ const openai = new OpenAI({
 
 router.use(validateToken);
 
+// Helpers
+
+function shouldAskAI() {
+    let difference =  new Date().getTime() - lastAiRequest.getTime(); // Diferencia en milis
+    return difference > 12 * 60 * 60 * 1000; // 12 horas
+}
+
+// End Helpers
+
+let lastAiRequest;
+
 const products = require('../mock/products');
 const orders = require('../mock/orders');
 const future_orders = require('../mock/future_orders');
@@ -33,35 +44,68 @@ router.get('/', function(req, res, next) {
 // GET future predictions on orders
 
 router.get('/predictions', async function(req, res, next) {
+
+    if (lastAiRequest != null && !shouldAskAI()) {
+        try {
+            let conn = await getConnection();
+            let request = await conn.request()
+                .execute('dbo.sp_GetOpportunities');
+            return res.success(
+                JSON.parse(request.recordset[0].content)
+            )
+        } catch (error) {
+            response = null;
+            console.log(error);
+            return res.error(500);
+        }
+    }
+
     console.log(`REQUEST: Analyze the next JSON object and create some sales oportunities for each client: ${JSON.stringify(orders)}`);
+    let response;
 
-    let response = await openai.chat.completions.create({
-        model: "gpt-4o",
-        messages: [
-            {
-                "role": "user",
-                "content": [
-                    {
-                        "type": "text",
-                        "text": `Based on this orders data: ${JSON.stringify(orders)} \n I would like for you to generate some sales opportunities and date estimates (when will each client make another order based on the history he has, meaning that the proposal dates shouldnt be the same for each client) for the clients that appear in such data, and if possible, i would like for you to format it like the next JSON, giving each sale opportunity a unique id:  \n ${JSON.stringify(future_orders)}, remember to take into account that the products data, excluding the total price and the quantity, are in the following json, and should not be modified: ${JSON.stringify(products)}`
-                    }
-                ]
-            }
-        ],
-        response_format: {
-            "type": "json_object"
-        },
-        store: true,
-        max_completion_tokens: 16384,
-        temperature: 1.0,
-        top_p: 1,
-        frequency_penalty: 0,
-        presence_penalty: 0
-    });
+    try {
+        response = await openai.chat.completions.create({
+            model: "gpt-4o",
+            messages: [
+                {
+                    "role": "user",
+                    "content": [
+                        {
+                            "type": "text",
+                            "text": `Based on this orders data: ${JSON.stringify(orders)} \n I would like for you to generate some sales opportunities and date estimates (when will each client make another order based on the history he has, the proposal dates could be the same for 2 clients) for the clients that appear in such data, and if possible, i would like for you to format it like the next JSON, giving each sale opportunity a unique id:  \n ${JSON.stringify(future_orders)}, remember to take into account that the products data, excluding the total price and the quantity, are in the following json, and should not be modified: ${JSON.stringify(products)}`
+                        }
+                    ]
+                }
+            ],
+            response_format: {
+                "type": "json_object"
+            },
+            store: true,
+            max_completion_tokens: 16384,
+            temperature: 1.0,
+            top_p: 1,
+            frequency_penalty: 0,
+            presence_penalty: 0
+        });
 
-    return res.success({
-        future_orders: JSON.parse(response.choices[0].message.content).future_orders
-    })
+        let aiOpportunities = response.choices[0].message.content;
+        lastAiRequest = new Date();
+
+        let conn = await getConnection();
+        let request = await conn.request()
+            .input("opp_content", sql.NVarChar(sql.MAX), aiOpportunities)
+            .execute('dbo.sp_InsertOpp');
+
+        return res.success({
+            future_orders: JSON.parse(aiOpportunities).future_orders
+        });
+
+    } catch (error) {
+        response = null;
+        console.log(error);
+        return res.error(500);
+    }
+
 
 });
 
